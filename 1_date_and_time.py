ba585c7b89b9d219eadcd8b2f47dda3953a10d83
Dilshat Mukhambetaliev <d.mukhambetaliev@facct.ru>
2024-09-20 20:59:34 +0300

    date_and_time done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,21 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
+import locale
+locale.setlocale(locale.LC_ALL, "ru_RU.UTF8")
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    date_now = datetime.now()
+    date_yesterday = date_now - timedelta(days=1)
+    date_30_days_ago = date_now - timedelta(days=30)
+    
+    print(f"Дата вчера: {date_yesterday.strftime('%A%d-%B-%Y')}\nДата сегодня: {date_now.strftime('%A:%d-%B-%Y')}\nДата 30 дней назад: {date_30_days_ago.strftime('%A:%d-%B-%Y')} ")
 
 
 def str_2_datetime(date_string):
@@ -21,8 +29,10 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+   
+    return datetime.strptime(date_string, '%m/%d/%Y %H:%M:%S.%f')
+    
 
 if __name__ == "__main__":
     print_days()
-    print(str_2_datetime("01/01/20 12:10:03.234567"))
+    print(str_2_datetime('01/01/2020 12:10:03.234567'))
