0494b2fe85a960c0b7567dc477db6b58a0507c47
aka-nomad <dilshat.mukhambetaliev@gmail.com>
2024-09-21 21:42:41 +0300

    2_file.py task done

--- 2_files.py
+++ 2_files.py
@@ -9,14 +9,26 @@
 3. Подсчитайте количество слов в тексте
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
-"""
+""" 
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(len(content))
+        words = content.split()
+        print(len(words))
+        
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.replace('.', '!')
+            with open('referat2.txt', 'a', encoding='utf-8') as new_file:
+                new_file.write(line)
+    
 
 if __name__ == "__main__":
     main()
