c3c1928c78a5e269564fc73f4a202521795e8ca5
aka-nomad <dilshat.mukhambetaliev@gmail.com>
2024-09-27 01:31:27 +0300

    csv task done

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -15,7 +15,31 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    import csv
+
+
+    dicts_list = [
+        { 
+            "name":"John", "age": 50, "job": "teacher"
+        },
+        {
+            "name":"Luis", "age": 25, "job": "software engineer"
+        },
+        {
+            "name":"Peter", "age": 44, "job": "manager"
+        },
+        {
+            "name":"Jesica", "age": 30, "job": "sales manager"
+        }
+    ]
+    print(dicts_list)
+    with open ('export.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for user in dicts_list:
+            writer.writerow(user)
+
 
 if __name__ == "__main__":
     main()
